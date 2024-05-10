a64039abd7846bf7ce4c5814f0a3303f123c7f8e
Manuel <manuelcendonr@gmail.com>
2024-05-10 10:16:30 +0200

    First Commit

---
+++ app/src/main/java/GitManu/Sumador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GitManu;
+
+/**
+ *
+ * @author a23manuelcr
+ */
+public class Sumador {
+    public static float sumar (float a, float b ) {
+return a + b;
+}
+}
